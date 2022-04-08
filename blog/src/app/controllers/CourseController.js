b1ecd2a15a677f61e8a120fbc8cfa51043c05811
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2022-04-08 19:05:25 +0700

    commit blog

---
+++ blog/src/app/controllers/CourseController.js
@@ -0,0 +1,32 @@
+const Course = require('../models/Course')
+const { mongooseToObject } = require('../../util/mongoose')
+
+class CourseController {
+    // [GET] /course/:slug
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then(course => 
+                res.render('courses/show', { course: mongooseToObject(course) })    
+            )
+            .catch(next);
+    }
+
+    // [GET] /course/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+    // [POST] /course/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `http://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        const course = new Course(req.body);
+        course.save()
+            .then(() => res.redirect(`/`))
+            .catch(error => {
+
+            });
+        
+    }
+}
+
+module.exports = new CourseController();
